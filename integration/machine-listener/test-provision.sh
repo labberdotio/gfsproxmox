bd1123c93e7e56843c4966755cae52b560a2d6d6
Jeremy Kuhnash <kuhnash@gmail.com>
2021-03-17 16:24:34 -0700

    updates to current state so John can check it out.

--- integration/machine-listener/test-provision.sh
+++ integration/machine-listener/test-provision.sh
@@ -2,15 +2,11 @@
 
 API_NODE="192.168.0.180"
 TARGET_NODE="botcore"
+TOKEN_ID='bots@pam!botstoken'
+TOKEN_SECRET='2b29934d-d95e-48e4-9050-f3df9a083c5b'
+REMOTE_NODE_ENDPOINT="https://$API_NODE:8006/api2/json/nodes/$TARGET_NODE"
 
-AUTH_Cookie=$(curl --silent --insecure --data "username=bots@pam&password=bots123" \
-    https://$API_NODE:8006/api2/json/access/ticket \
-    | jq --raw-output '.data.ticket' | sed 's/^/PVEAuthCookie=/')
+echo "Authorization: PVEAPIToken=${TOKEN_ID}=${TOKEN_SECRET}"
 
-CSRF_Token=$(curl --silent --insecure --data "username=bots@pam&password=bots123" \
-    https://$API_NODE:8006/api2/json/access/ticket \
-    | jq --raw-output '.data.CSRFPreventionToken' | sed 's/^/CSRFPreventionToken:/')
-
-# curl  --insecure --cookie $AUTH_Cookie https://$API_NODE:8006/api2/json/nodes/$TARGET_NODE/status
-
-curl --silent --insecure --cookie $AUTH_Cookie https://$API_NODE:8006/api2/json/nodes/botcore/qemu | jq '.'
+curl --location --insecure --header "Authorization: PVEAPIToken=${TOKEN_ID}=${TOKEN_SECRET}" \
+    --request GET $REMOTE_NODE_ENDPOINT
